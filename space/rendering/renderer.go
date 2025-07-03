803815c2ba6273a6139a115d18b7260d95754a43
flo <sabaniflorian@gmail.com>
2025-07-03 16:51:03 +0200

    disabled debug raycast

--- space/rendering/renderer.go
+++ space/rendering/renderer.go
@@ -451,7 +451,7 @@ func (r *Renderer) renderShadows(screen *ebiten.Image, model *Models.SpaceGame) 
 	lightDirection := camera.WorldToScreen(camera.Position, constants.ScreenWidth, constants.ScreenHeight)
 	fov := r.getAdaptiveFov(lightDirection, lightPos)
 
-	r.shadowSystem.RenderShadows(screen, lightPos, lightDirection, fov, camera.GetTotalZoom(), celestialPositions, celestialRadii, asteroidPositions, asteroidRadii, portalPositions, portalRadii, portalRotations, true)
+	r.shadowSystem.RenderShadows(screen, lightPos, lightDirection, fov, camera.GetTotalZoom(), celestialPositions, celestialRadii, asteroidPositions, asteroidRadii, portalPositions, portalRadii, portalRotations, false)
 }
 
 // drawTrajectoryArrow draws the trajectory arrow when dragging
